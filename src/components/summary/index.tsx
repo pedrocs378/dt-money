047a2aa786642b4e9fccc041c4b21faf2154e33e
Pedro <pedrocs378@gmail.com>
2022-08-08 21:51:03 -0300

    refactor: calc summary values based in transactions data

--- src/components/summary/index.tsx
+++ src/components/summary/index.tsx
@@ -1,8 +1,34 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { useMemo } from 'react'
+
+import { useTransactions } from '../../contexts/transactions-context'
 
 import * as S from './styles'
 
 export function Summary() {
+  const { transactions } = useTransactions()
+
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
+
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0
+      }
+    )
+  }, [transactions])
+
   return (
     <S.SummaryContainer>
       <S.SummaryCard>
@@ -11,7 +37,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b373" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </S.SummaryCard>
       <S.SummaryCard>
         <header>
@@ -19,7 +45,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </S.SummaryCard>
       <S.SummaryCard variant="green">
         <header>
@@ -27,7 +53,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </S.SummaryCard>
     </S.SummaryContainer>
   )
